56a85052dbe40272d3af3b987b352986b84473d8
Baranga69 <69077312+Baranga69@users.noreply.github.com>
2022-05-24 22:59:04 +0300

    Creation of the offers page

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__, template_folder="templates/Main")
 
 
-if __name__ == '__main__':
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/offers")
+def page():
+    return render_template("offers.html")
+
+
+if __name__ == "__main__":
     app.run()
