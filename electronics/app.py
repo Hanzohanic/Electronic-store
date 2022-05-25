fbd36085482f67e514b049030845601ba22fe005
Hanzohanic <hanzohanic@gmail.com>
2022-05-25 13:50:58 +0200

    v3

--- electronics/app.py
+++ electronics/app.py
@@ -3,14 +3,14 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__, template_folder="templates/Main")
 
 
-@app.route("/index")
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
 @app.route("/offers")
 def page():
-    return render_template("offers.html")
+    return render_template("storefront/offers.html")
 
 
 if __name__ == "__main__":
